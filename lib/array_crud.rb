6463c5ab981d38317bba1b696ed1d18091b6f294
Brandon James <brandonj2858@gmail.com>
2019-05-20 13:32:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.unshift("Michael")
+  array.unshift("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
