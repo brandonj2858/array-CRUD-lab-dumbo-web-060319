4c591e3056c2fe6546ee22bbc80d6f9cff67a597
Brandon James <brandonj2858@gmail.com>
2019-05-20 13:30:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  pokemon = [pikachu, squirtle, bulbasaur, lugia]
+  pokemon = ["pikachu", "squirtle", "bulbasaur", "lugia"]
 end
 
 def add_element_to_end_of_array(array, element)
